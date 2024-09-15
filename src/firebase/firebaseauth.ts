d21dd5c9f58a5b61f73a8164d3cd322f642cb3ac
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-09-16 00:00:13 +0500

    removed commented lines

--- src/firebase/firebaseauth.ts
+++ src/firebase/firebaseauth.ts
@@ -1,8 +1,7 @@
 import { app } from '@/firebase/firbaseconfig';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword ,GoogleAuthProvider , signInWithPopup} from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 
-// const provider = new GoogleAuthProvider();
 
 
 const auth = getAuth(app);
@@ -44,31 +43,3 @@ export function loginWithEmailPassword(email: string, password: string) {
 
         });
 }
-
-
-
-// export function googleSign(auth,provider) {
-//     signInWithPopup(auth, provider)
-//   .then((result) => {
-//     // This gives you a Google Access Token. You can use it to access the Google API.
-//     const credential = GoogleAuthProvider.credentialFromResult(result);
-//     const token = credential.accessToken;
-//     // The signed-in user info.
-//     const user = result.user;
-//     // IdP data available using getAdditionalUserInfo(result)
-//     console.log(credential, token ,user);
-    
-//     // ...
-//   }).catch((error) => {
-//     // Handle Errors here.
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // The email of the user's account used.
-//     const email = error.customData.email;
-//     // The AuthCredential type that was used.
-//     const credential = GoogleAuthProvider.credentialFromError(error);
-//     console.log(errorCode,errorMessage, email, credential);
-    
-//     // ...
-//   });
-// }
